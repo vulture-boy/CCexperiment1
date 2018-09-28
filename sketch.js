24d4e130b04ac6634d8d0f5ea902111a124b72a7
Tyson Moll (vultures) <tyson.moll@mail.utoronto.ca>
2018-09-28 14:05:10 -0400

    Prepared (commented) outline of project scope
    see sketch.js

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	// Recording Button
+		// File to Save recording to
+		// Recording operation
+		// Time limitation
+		// Graphical Representation
+		
+	// Sliders
+		// Volume
+		// Delay
+		// Filter
+		// Reverb
+		// EQ?
+		// Fade? 
+		// Graphical Representation
+		// BPM
+		// Associated manipulation of sound (non-destructive)
+		
+	// Sample Matrix
+		// Array of sequence buttons
+		// Array for representation
+		// Counter to move through playing items in array
+		// BPM to Counter Value conversion (& display)
+		// 
+		
+	// GUI (last job)
+		// Mockup of layout (photoshop/illustrator/etc)
+		// Graphical Overhaul & Elements
+		// Adaptive positions / sizing
+		
+}
+
+function windowResized() {
+	canvasWidth = windowWidth;
+	canvasHeight = windowHeight;
+}
