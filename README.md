b026e73eaeab656b07d81b2afc1c7316003cec89
Tyson Moll (vultures) <tyson.moll@mail.utoronto.ca>
2018-10-01 16:36:05 -0400

    Moved doc comments to readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# CCexperiment1
+# OctoSwish Plus - A graphical audio sampler
+Sample Board by Tyson Moll
+Audiovisualization by Amreen
+
+for Creation and Computation class, Fall 2018
+Digital Futures Graduate Program
+
+### KNOWN ISSUES:
+- Playback buttons are probably backwards / not matching use
+- Proportional scaling has some issues... 
+- There's backend functionality for portrait mode, but not used yet
+- Playback needs a check to ensure something has been recorded
+- Board Buttons need to be redone such that the highlight is seperate
+- Board Buttons need a press state
+
+### NOT YET IMPLEMENTED:
+- Recording
+	// Time Display
+	
+- Playback (board)
+	// BPM to Counter Value conversion (& display)
+- Sound Manipulation sliders
+	// Volume
+	// Delay
+	// Filter
+	// Reverb
+	// EQ?
+	// Fade? 
+	// Cozy with GUI
+	// (confirm non-destructive)
+	// "Tilt Whammy" + buttons
+- Proper Proportional Scaling
+- Audiovisual 
+	// Button to toggle mode
+	// Display Mode functionality (to confirm when complete)
+	// (Amreen to do the audiovisual display)
--- sketch.js
+++ sketch.js
@@ -1,43 +1,5 @@
 /* OctoSwish Plus - A graphical audio sampler
-Sample Board by Tyson Moll
-Audiovisualization by Amreen
-
-for Creation and Computation class, Fall 2018
-Digital Futures Graduate Program
-
-KNOWN ISSUES:
-- Playback buttons are probably backwards / not matching use
-- Proportional scaling has some issues... 
-- There's backend functionality for portrait mode, but not used yet
-- Playback needs a check to ensure something has been recorded
-- Board Buttons need to be redone such that the highlight is seperate
-- Board Buttons need a press state
-
-NOT YET IMPLEMENTED:
-- Recording
-	// Time Display
-	
-- Playback (sample)
-- Playback (board)
-	// Play Matrix Button
-	// Counter to move through playing items in array (playback cursor)
-	// BPM to Counter Value conversion (& display)
-- Sound Manipulation sliders
-	// Volume
-	// Delay
-	// Filter
-	// Reverb
-	// EQ?
-	// Fade? 
-	// Cozy with GUI
-	// (confirm non-destructive)
-	// "Tilt Whammy" + buttons
-- Proper Proportional Scaling
-- Audiovisual 
-	// Button to toggle mode
-	// Display Mode functionality (to confirm when complete)
-	// (Amreen to do the audiovisual display)
-
+See README for details
 */
 
 function preload() { // Preload graphical assets
@@ -145,7 +107,45 @@ function setup() { // Initialization of Canvas Properties
 	// Setup relative button positions
 	calibrateButtons()
 	
+	// Selector
+	selWidth = '180px';
+	selector = createSelect();
+	selector.position(10,10);
+	selector.option('Volume');
+	selector.option('Delay');
+	selector.option('Filter');
+	selector.option('Reverb');
+	selector.changed(selectorEvent);
+	selector.style('width', selWidth);
 	
+	// Slider
+		// STUB: proportionals
+	slider = createSlider(0,255,0,1);
+	slider.position(10,40);
+	slider.style('width', selWidth);
+	
+	/*
+	masterVolume(vol) - 0 to 1 (0.5: default)
+	.setVolume(vol) - 0 to 1 (0.5: default)
+	.drywet(val) - 0 to 1
+	*/
+	
+	
+}
+
+function numToTime(num) { // Converts an int to a time string
+	
+	// STUB: Incomplete
+	timeReadout = nf(num);
+	if (timeReadout.length == 1) {
+		timeReadout = "0" + timeReadout;
+	}
+	return timeReadout;
+}
+
+function selectorEvent() {
+	var item = selector.value();
+	// STUB: Modify slider values / target
 }
 
 function Button(xOrigin,yOrigin,proport, xPos, yPos) { // Standard Button Object
