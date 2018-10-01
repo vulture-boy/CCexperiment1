8c051a0c18284474e01f8f832095290e8244ac06
Tyson Moll (vultures) <tyson.moll@mail.utoronto.ca>
2018-10-01 17:46:59 -0400

    Attempting to fix github bug... again!

--- sketch.js
+++ sketch.js
@@ -3,22 +3,22 @@ See README for details
 */
 
 function preload() { // Preload graphical assets
-	img_board_on = loadImage('./images/button_gfx/board/board_in.png');
-	img_board_off = loadImage('/images/button_gfx/board/board_out.png');
-	img_board_press = loadImage('/images/button_gfx/board/board_out.png');
-	img_board_on_act = loadImage('/images/button_gfx/board/board_inA.png');
-	img_board_off_act = loadImage('/images/button_gfx/board/board_outA.png');
-	img_board_press_act = loadImage('/images/button_gfx/board/board_outA.png');
-	img_cursor = loadImage('/images/button_gfx/board/cursor.png');
-	img_mic_on = loadImage('/images/button_gfx/mic/mic_on.png');
-	img_mic_off = loadImage('/images/button_gfx/mic/mic_off.png');
-	img_mic_press = loadImage('/images/button_gfx/mic/mic_press.png');
-	img_play_board_on = loadImage('/images/button_gfx/play/play_on.png');
-	img_play_board_off = loadImage('/images/button_gfx/play/play_off.png');
-	img_play_board_press = loadImage('/images/button_gfx/play/play_press.png');
-	img_play_sample_on = loadImage('/images/button_gfx/play/playS_on.png');
-	img_play_sample_off = loadImage('/images/button_gfx/play/playS_off.png');
-	img_play_sample_press = loadImage('/images/button_gfx/play/playS_press.png');
+	img_board_on = loadImage('./images/button/board/board_in.png');
+	img_board_off = loadImage('/images/button/board/board_out.png');
+	img_board_press = loadImage('/images/button/board/board_out.png');
+	img_board_on_act = loadImage('/images/button/board/board_inA.png');
+	img_board_off_act = loadImage('/images/button/board/board_outA.png');
+	img_board_press_act = loadImage('/images/button/board/board_outA.png');
+	img_cursor = loadImage('/images/button/board/cursor.png');
+	img_mic_on = loadImage('/images/button/mic/mic_on.png');
+	img_mic_off = loadImage('/images/button/mic/mic_off.png');
+	img_mic_press = loadImage('/images/button/mic/mic_press.png');
+	img_play_board_on = loadImage('/images/button/play/play_on.png');
+	img_play_board_off = loadImage('/images/button/play/play_off.png');
+	img_play_board_press = loadImage('/images/button/play/play_press.png');
+	img_play_sample_on = loadImage('/images/button/play/playS_on.png');
+	img_play_sample_off = loadImage('/images/button/play/playS_off.png');
+	img_play_sample_press = loadImage('/images/button/play/playS_press.png');
 	
 }
 
