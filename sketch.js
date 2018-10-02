6724b26bb666c1e119b10207242bf7396dbd7186
vultures <t.moll@outlook.com>
2018-10-02 15:15:23 -0400

    same

--- sketch.js
+++ sketch.js
@@ -3,22 +3,22 @@ See README for details
 */
 
 function preload() { // Preload graphical assets
-	img_board_on = loadImage('./images/boardIn.png');
-	img_board_off = loadImage('./images/boardOut.png');
-	img_board_press = loadImage('./images/boardOut.png');
-	img_board_on_act = loadImage('./images/boardInA.png');
-	img_board_off_act = loadImage('./images/boardOutA.png');
-	img_board_press_act = loadImage('./images/boardOutA.png');
-	img_cursor = loadImage('./images/cursor.png');
-	img_mic_on = loadImage('./images/micOn.png');
-	img_mic_off = loadImage('./images/micOff.png');
-	img_mic_press = loadImage('./images/micPress.png');
-	img_play_board_on = loadImage('./images/playOn.png');
-	img_play_board_off = loadImage('./images/playOff.png');
-	img_play_board_press = loadImage('./images/playPress.png');
-	img_play_sample_on = loadImage('./images/playSOn.png');
-	img_play_sample_off = loadImage('./images/playSOff.png');
-	img_play_sample_press = loadImage('./images/playSPress.png');
+	img_board_on = loadImage('CCexperiment1/images/boardIn.png');
+	img_board_off = loadImage('CCexperiment1/images/boardOut.png');
+	img_board_press = loadImage('CCexperiment1/images/boardOut.png');
+	img_board_on_act = loadImage('CCexperiment1/images/boardInA.png');
+	img_board_off_act = loadImage('images/boardOutA.png');
+	img_board_press_act = loadImage('images/boardOutA.png');
+	img_cursor = loadImage('images/cursor.png');
+	img_mic_on = loadImage('images/micOn.png');
+	img_mic_off = loadImage('images/micOff.png');
+	img_mic_press = loadImage('images/micPress.png');
+	img_play_board_on = loadImage('images/playOn.png');
+	img_play_board_off = loadImage('images/playOff.png');
+	img_play_board_press = loadImage('images/playPress.png');
+	img_play_sample_on = loadImage('images/playSOn.png');
+	img_play_sample_off = loadImage('images/playSOff.png');
+	img_play_sample_press = loadImage('images/playSPress.png');
 	
 }
 
