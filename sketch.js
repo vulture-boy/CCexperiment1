3aab456f1ec34bb229616eb631c66a7994f62e97
Tyson Moll (vultures) <tyson.moll@mail.utoronto.ca>
2018-10-04 23:38:07 -0400

    Audiovisual mode incorporated into project

--- sketch.js
+++ sketch.js
@@ -51,6 +51,22 @@ function setup() { // Initialization of Canvas Properties
 	nativeX = 1920; // Mockup proportions
 	nativeY = 1080;
 	
+	////// AUDIOVISUAL //////
+	
+	volHistory = []; // Stores volume values 
+	backstep = 2; // Time backstep to draw circles (offset)
+	circles = 10; // Circles to draw
+	brighter = 12; // Brightness difference between circles
+	multiplierA = windowWidth * 3; // Width multiplier for amplitude
+	multiplierB = windowHeight * 3; // Height multiplier for amplitude
+	
+	redInit = random(110); // Initialize Unique Colours for each instance
+	blueInit = random(110);
+	greenInit = random(110);
+	
+    calibrateWindow();
+	
+	
 	////// Sound //////
 	
 	// Initialize mic device 
@@ -391,10 +407,49 @@ function draw() { // Occurs each frame
 				sampleButtons[i].display();
 			}
 		
-		// Audiovisual Mode
+		// Audiovisual Mode (Amreen)
 		} else {
-			background(250);
-			// STUB
+			background(redInit+10,blueInit+10,greenInit+10);
+			var vol = mic.getLevel();
+			console.log(vol);
+			volHistory.push(vol);
+			
+			// Circles 
+			for (i=0;i<(circles);i++) {
+				if (volHistory.length > backstep * i ) {
+					push();
+						beginShape();
+						translate(0,0); // Confirm position
+						noStroke();
+						//fill(random(100)+i*brighter,random(100)+i*brighter,random(100)+i*brighter);  // Trippy edition
+						fill(redInit +i*brighter,blueInit +i*brighter,greenInit +i*brighter, 180);
+						var backValue = volHistory.length -backstep * i -1; // Where to source amp data from
+						ellipse(shapePositionX,shapePositionY,volHistory[backValue] * multiplierA, volHistory[backValue] * multiplierB);
+						endShape();
+					pop();
+
+				}
+			}
+			
+			// Lines
+			noFill();
+			push();
+				beginShape();
+				var currentY = map(vol, 0, 1, height, 0);
+				translate(0, height / 2 - currentY);
+				stroke(255-redInit,255-blueInit,255-greenInit);
+				strokeWeight(2);
+				for (var i = 0; i < volHistory.length; i++) {
+					var y = map(volHistory[i], 0, 1, height, 0);
+					
+					vertex(i, y);
+					line(i,y,i,windowHeight);
+				}
+				endShape();
+			pop();
+			if (volHistory.length > width) {
+				volHistory.splice(0, 1);
+			}
 		}
 	}
 	//////+ BUTTONS +////// 
@@ -606,5 +661,12 @@ function mousePressed() {
 
 function windowResized() { // Triggered when window is resized
 	resizeCanvas(windowWidth,windowHeight);
+	calibrateWindow();
 	selectorUpdate();
+}
+
+function calibrateWindow() { // Calibrates visualizer
+    shapePositionX = windowWidth/2;
+    shapePositionY = windowHeight/2;
+    shapeWidth = windowWidth/5;
 }
